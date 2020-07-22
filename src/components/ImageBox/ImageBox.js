4e85ff7a97c894c06c56475803447cdc4e94afd9
SaranshDawra <saransh2000dawra@gmail.com>
2020-07-22 21:27:47 +0530

    Image Component Added

--- src/components/ImageBox/ImageBox.js
+++ src/components/ImageBox/ImageBox.js
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
+import classes from "./ImageBox.module.css";
 
 class ImageBox extends Component {
     render() {
         return (
-            <div>
+            <div className={classes.ImageBox}>
+                <ul className={classes.List}>
+                    {this.props.items.map((item) => {
+                        const tag = item.tags.split(',');
+                        return (
+                            <li key={item.id} className={classes.ListEl}>
+                                <img
+                                    className={classes.Image}
+                                    width={item.webformatWidth / 2}
+                                    height={item.webformatHeight}
+                                    src={item.webformatURL}
+                                    alt={tag[0]}
+                                />
+                            </li>
+                        );
+                    })}
+                </ul>
             </div>
         );
     }
